f32bd89c8897721a73aec887178e33e038ec7472
Chaojian Zhang <szinubuntu@gmail.com>
2024-06-11 09:51:27 -0400

    General refactor

--- PracticeBareboneHTTPServer/Program.cs
+++ PracticeBareboneHTTPServer/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Sockets;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PracticeBareboneHTTPServer
 {
@@ -8,13 +7,15 @@ namespace PracticeBareboneHTTPServer
     {
         static void Main(string[] args)
         {
-            const int port = 9000;
-            TcpListener server = new TcpListener(System.Net.IPAddress.Any, port);
+            // Single argument
+            int port = int.Parse(args.Single());
+            const int bufferSize = 2048;
+
+            TcpListener server = new(System.Net.IPAddress.Any, port);
             server.Start();
 
-            // Buffer for reading data
-            byte[] bytes = new byte[2048];
-
+            // Main server loop
+            byte[] bytes = new byte[bufferSize]; // Buffer for reading data
             while (true)
             {
                 Console.Write("Waiting for a connection... ");
@@ -29,7 +30,7 @@ namespace PracticeBareboneHTTPServer
                 while (stream.DataAvailable && (i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
                     // Translate data bytes to a ASCII string.
-                    string data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                    string data = Encoding.UTF8.GetString(bytes, 0, i);
                     Console.WriteLine($"Received: {data}");
                     sb.Append(data);
                 }
@@ -38,37 +39,21 @@ namespace PracticeBareboneHTTPServer
                 Console.WriteLine($"Complete message: {requestHeader}");
 
                 // Send reply
-                string body = $"""
-                    <!DOCTYPE html>
-                    <html lang="en">
+                byte[] replyData = MakeReply(requestHeader);
+                stream.Write(replyData);
+            }
+        }
 
-                    <head>
-                      <meta charset="utf-8">
-                      <meta name="viewport" content="width=device-width, initial-scale=1">
-                      <title>HTML5 Boilerplate</title>
-                      <link rel="stylesheet" href="styles.css">
-                    </head>
-
-                    <body>
-                      <h1>Page Title</h1>
-                      <script src="scripts.js"></script>
-                    </body>
-
-                    </html>
-                    """;
-                string replyHeader = $"""
-                    HTTP/1.1 200 OK
-                    Content-Length: {body.Length}
-                    Content-Type: text/plain; charset=utf-8
-                    """;
-                string reply = $"""
+        private static byte[] MakeReply(string requestHeader)
+        {
+            ReplyHandler.HandleReply(requestHeader, out string body, out string replyHeader);
+            string reply = $"""
                     {replyHeader}
                     
                     {body}
                     """;
-                byte[] replyData = System.Text.Encoding.UTF8.GetBytes(reply);
-                stream.Write(replyData);
-            }
+            byte[] replyData = Encoding.UTF8.GetBytes(reply);
+            return replyData;
         }
     }
 }
---
+++ PracticeBareboneHTTPServer/ReplyHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace PracticeBareboneHTTPServer
+{
+    public class ReplyHandler
+    {
+        public static void HandleReply(string requestHeader, out string body, out string replyHeader)
+        {
+            body = $"""
+                    <!DOCTYPE html>
+                    <html lang="en">
+
+                    <head>
+                      <meta charset="utf-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <title>HTML5 Boilerplate</title>
+                      <link rel="stylesheet" href="styles.css">
+                    </head>
+
+                    <body>
+                      <h1>Page Title</h1>
+                      <script src="scripts.js"></script>
+                    </body>
+
+                    </html>
+                    """;
+            replyHeader = $"""
+                    HTTP/1.1 200 OK
+                    Content-Length: {body.Length}
+                    Content-Type: text/plain; charset=utf-8
+                    """;
+        }
+    }
+}
